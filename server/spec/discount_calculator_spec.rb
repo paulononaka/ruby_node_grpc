12b1cb73209686d346516407428e0c2992d662ee
Paulo Henrique Nonaka <paulononaka@gmail.com>
2020-02-23 14:23:25 -0300

    Refactors Ruby tests

--- server/spec/discount_calculator_spec.rb
+++ server/spec/discount_calculator_spec.rb
@@ -5,41 +5,20 @@ require 'date'
 
 RSpec.describe DiscountCalculatorService do
 
-  before do
-    Ruson.output_folder = './spec/data/'
-
-    @product = Product.create(id: '1',
-                              price_in_cents: 200,
-                              title: 'Some product title',
-                              description: 'Some product description')
-
-    @discount_calculator = DiscountCalculatorService.new
-  end
-
-  after(:all) { FileUtils.rm_rf('./spec/data/') }
-
   context "when calling get_discount" do
-    it "returns a proto Proto::Product" do
-      discount_request = Proto::GetDiscountRequest.new(product_id: @product.id, user_id: '123')
 
-      result = @discount_calculator.get_discount(discount_request, nil)
-
-      expect(result).to be_a_kind_of(Proto::Product)
+    before do
+      Ruson.output_folder = './spec/data/'
+      @discount_calculator = DiscountCalculatorService.new
     end
 
-    it "finds and returns the product passed by the request parameters" do
-      discount_request = Proto::GetDiscountRequest.new(product_id: @product.id, user_id: '123')
-
-      result = @discount_calculator.get_discount(discount_request, nil)
-
-      expect(result.id).to eq(@product.id)
-      expect(result.price_in_cents).to eq(@product.price_in_cents)
-      expect(result.title).to eq(@product.title)
-      expect(result.description).to eq(@product.description)
+    after(:all) do
+      FileUtils.rm_rf('./spec/data/')
     end
 
     it "returns a empty Proto::Product when can not find product in the database" do
-      discount_request = Proto::GetDiscountRequest.new(product_id: '999', user_id: '123')
+      FileUtils.rm_rf('./spec/data/')
+      discount_request = Proto::GetDiscountRequest.new(product_id: '999', user_id: '999')
 
       result = @discount_calculator.get_discount(discount_request, nil)
 
@@ -47,30 +26,59 @@ RSpec.describe DiscountCalculatorService do
       expect(result.id).to be_empty
     end
 
-    it "the product has no discount if can not find user" do
-      discount_request = Proto::GetDiscountRequest.new(product_id: @product.id, user_id: '999')
-
-      result = @discount_calculator.get_discount(discount_request, nil)
-
-      expect(result.discount.pct).to eq(0)
-      expect(result.discount.value_in_cents).to eq(0)
-    end
-
-    context "there is a user in the database celebrating an anniversary" do
+    context "there is a product in the database with price 200" do
       before do
-        @user = User.create(id: '1',
-                            first_name: 'Bart',
-                            last_name: 'Simpson',
-                            date_of_birth: Date.today - 10.years)
+        @product = Product.create(id: '1',
+                                  price_in_cents: 200,
+                                  title: 'Some product title',
+                                  description: 'Some product description')
       end
 
-      it "the product has 5% discount if it's the user's birthday" do
-        discount_request = Proto::GetDiscountRequest.new(product_id: @product.id, user_id: @user.id)
+      it "returns a proto Proto::Product" do
+        discount_request = Proto::GetDiscountRequest.new(product_id: @product.id, user_id: '123')
 
         result = @discount_calculator.get_discount(discount_request, nil)
 
-        expect(result.discount.pct).to eq(5)
-        expect(result.discount.value_in_cents).to eq(190)
+        expect(result).to be_a_kind_of(Proto::Product)
+      end
+
+      it "finds and returns the product passed by the request parameters" do
+        discount_request = Proto::GetDiscountRequest.new(product_id: @product.id, user_id: '123')
+
+        result = @discount_calculator.get_discount(discount_request, nil)
+
+        expect(result.id).to eq(@product.id)
+        expect(result.price_in_cents).to eq(@product.price_in_cents)
+        expect(result.title).to eq(@product.title)
+        expect(result.description).to eq(@product.description)
+      end
+
+      it "the product has no discount if can not find user" do
+        FileUtils.rm_rf('./spec/data/Users/')
+        discount_request = Proto::GetDiscountRequest.new(product_id: @product.id, user_id: '999')
+
+        result = @discount_calculator.get_discount(discount_request, nil)
+
+        expect(result.discount.pct).to eq(0)
+        expect(result.discount.value_in_cents).to eq(0)
+      end
+
+      context "there is a user in the database celebrating an anniversary" do
+        before do
+          @user = User.create(id: '1',
+                              first_name: 'Bart',
+                              last_name: 'Simpson',
+                              date_of_birth: Date.today - 10.years)
+        end
+
+        it "the product has 5% discount if it's the user's birthday" do
+          discount_request = Proto::GetDiscountRequest.new(product_id: @product.id, user_id: @user.id)
+
+          result = @discount_calculator.get_discount(discount_request, nil)
+
+          expect(result.discount.pct).to eq(5)
+          expect(result.discount.value_in_cents).to eq(190)
+        end
       end
     end
   end
